a51dd75fb2828882232ec90e4c2f113a0966379e
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2025-03-10 10:12:57 +0300

    add modals

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,20 @@ import { tabs } from "./modules/tabs.js";
 import { accordionFunc } from "./modules/accordion.js";
 import { swiper } from "./modules/swiper.js";
 import { map } from "./modules/map.js";
+import { modals } from "./modules/modals.js";
+import IMask from "imask";
 
 accordionFunc();
 tabs();
+modals();
 swiper();
 map();
 
 import "../pug/components/selector/selector.js";
 import "../pug/pages/info/info-content/info-content.js";
+
+document.querySelectorAll(".input__valid-phone").forEach((el) => {
+  IMask(el, {
+    mask: "+7 (000) 000-00-00",
+  });
+});
