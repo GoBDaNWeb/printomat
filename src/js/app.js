ffdb1937621feb60d247f3eaf1593616ad38402e
cmroonn <fallgetell@gmail.com>
2025-03-10 10:24:42 +0300

    profile pages

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,15 @@ import { tabs } from "./modules/tabs.js";
 import { accordionFunc } from "./modules/accordion.js";
 import { swiper } from "./modules/swiper.js";
 import { map } from "./modules/map.js";
+import { modals } from "./modules/modals.js";
 
 accordionFunc();
 tabs();
 swiper();
 map();
+modals();
 
 import "../pug/components/selector/selector.js";
 import "../pug/pages/info/info-content/info-content.js";
+import "../pug/pages/profile/orders/orders.js";
+import "../pug/components/popup/popup.js";
---
+++ src/pug/pages/profile/orders/orders.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const handlers = document.querySelectorAll(".orders__order__handle");
+
+	handlers.forEach(btn => {
+		btn.addEventListener("click", (e) => {
+			e.preventDefault();
+			let parent = btn.closest(".orders__order");
+			parent.classList.toggle("active");
+		});
+	});
+});
