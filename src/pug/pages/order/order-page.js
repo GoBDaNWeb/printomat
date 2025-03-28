cc265bfb35e95eb8b064a1f9d7c6f1c9901907ba
cmroonn <fallgetell@gmail.com>
2025-03-28 12:24:51 +0300

    mobile fixes

--- src/pug/pages/order/order-page.js
+++ src/pug/pages/order/order-page.js
@@ -1,4 +1,5 @@
 const handles = document.querySelectorAll(".order__right__item__name ");
+const orderBtn = document.querySelector(".order__right h6");
 
 handles.forEach(handle => {
 	handle.addEventListener("click", function() {
@@ -8,3 +9,13 @@ handles.forEach(handle => {
 		handle.classList.toggle("active");
 	});
 })
+
+try {
+	orderBtn.addEventListener("click", function() {
+		const parent = orderBtn.closest(".order__right");
+		parent.classList.toggle("active");
+		orderBtn.classList.toggle("active");
+	});
+} catch (e) {
+	console.log(e);
+}
