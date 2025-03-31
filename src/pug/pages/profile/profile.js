554581f2c4c45630beda920dfdf49592d8a9c7ad
cmroonn <fallgetell@gmail.com>
2025-03-31 14:53:55 +0300

    fixes

--- src/pug/pages/profile/profile.js
+++ src/pug/pages/profile/profile.js
@@ -9,7 +9,46 @@ blocks.forEach(block => {
 			let valid = [];
 			fields.forEach(field => {
 				field.value === '' ? valid.push(field) : false;
+				field.value === '' ? field.classList.add("error") : field.classList.remove("error");
+
+				if(field.classList.contains('input__valid-phone')) {
+					field.value.length < 18 ? valid.push(field) : false;
+					field.value.length < 18 ? field.classList.add("error") : field.classList.remove("error");
+		
+				}
+
+				if(field.classList.contains('input__valid-inn')) {
+					field.value.length < 11 ? valid.push(field) : false;
+					field.value.length < 11 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-inn')) {
+					field.value.length < 11 ? valid.push(field) : false;
+					field.value.length < 11 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-kpp')) {
+					field.value.length < 8 ? valid.push(field) : false;
+					field.value.length < 8 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-bik')) {
+					field.value.length < 8 ? valid.push(field) : false;
+					field.value.length < 8 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-corr')) {
+					field.value.length < 19 ? valid.push(field) : false;
+					field.value.length < 19 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-ras')) {
+					field.value.length < 19 ? valid.push(field) : false;
+					field.value.length < 19 ? field.classList.add("error") : field.classList.remove("error");
+				}
 			});
+
+			
 			if (valid.length > 0) {
 				saveBtn.setAttribute("disabled", "disabled");
 				return;
@@ -25,7 +64,45 @@ blocks.forEach(block => {
 			let valid = [];
 			fields.forEach(field => {
 				field.value === '' ? valid.push(field) : false;
+				field.value === '' ? field.classList.add("error") : field.classList.remove("error");
+
+				if(field.classList.contains('input__valid-phone')) {
+					field.value.length < 18 ? valid.push(field) : false;
+					field.value.length < 18 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-inn')) {
+					field.value.length < 11 ? valid.push(field) : false;
+					field.value.length < 11 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-inn')) {
+					field.value.length < 11 ? valid.push(field) : false;
+					field.value.length < 11 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-kpp')) {
+					field.value.length < 8 ? valid.push(field) : false;
+					field.value.length < 8 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-bik')) {
+					field.value.length < 8 ? valid.push(field) : false;
+					field.value.length < 8 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-corr')) {
+					field.value.length < 19 ? valid.push(field) : false;
+					field.value.length < 19 ? field.classList.add("error") : field.classList.remove("error");
+				}
+
+				if(field.classList.contains('input__valid-ras')) {
+					field.value.length < 19 ? valid.push(field) : false;
+					field.value.length < 19 ? field.classList.add("error") : field.classList.remove("error");
+				}
 			});
+
+			
 			if (valid.length > 0) {
 				saveBtn.setAttribute("disabled", "disabled");
 				return;
